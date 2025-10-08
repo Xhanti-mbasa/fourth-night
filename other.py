c4769d93e88eea4c990d870b960a9f06743c07be
xhmohcpt025 <xhmohcpt025@student.wethinkcode.co.za>
2025-10-08 21:08:21 +0200

    :finish 09/oct/2025

---
+++ other.py
@@ -0,0 +1,17 @@
+map = ["lake", "river", "mountain", "desert", "cave"]
+weapons = ["spoon", "knife", "spok"]
+current = ["field"]
+char = ["shatter"]
+
+
+print("welcome screen")
+
+
+# while True:
+#     user_input = input("where to? ")
+#     if user_input not  in map:
+#         print("input not in lists")
+#     else:
+#         maps = map.pop(map.index(user_input))
+#
+#         print(maps)
